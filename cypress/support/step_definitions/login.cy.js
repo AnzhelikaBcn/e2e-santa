97f1b54174b70dbd3efd52a37cf9c9df35e38a0a
Anzhelika <angela.pikulina@mail.com>
2024-01-16 22:10:49 +0100

    HW7_1_1_3

--- cypress/support/step_definitions/login.cy.js
+++ cypress/support/step_definitions/login.cy.js
@@ -1,24 +1,87 @@
-import { Given } from "@badeball/cypress-cucumber-preprocessor";
-const users = require("../../fixtures/users.json");
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import users from "../../fixtures/users.json";
+import boxPage from "../fixtures/pages/boxPage.json";
+import generalElements from "../fixtures/pages/general.json";
+import dashboardPage from "../fixtures/pages/dashboardPage.json";
+import invitePage from "../fixtures/pages/invitePage.json";
+import inviteeBoxPage from "../fixtures/pages/inviteeBoxPage.json";
+import inviteeDashboardPage from "../fixtures/pages/inviteeDashboardPage.json";
+import { faker } from "@faker-js/faker";
 
-Given("user is on secret santa login page", function () {
-    cy.visit("/login");
+let newBoxName, wishes, maxAmount, currency, inviteLink;
+
+Given("user is logged in", () => {
+  cy.visit("/login");
+  cy.login(users.userAutor.email, users.userAutor.password);
 });
 
+When("user creates a box", () => {
+  newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
+  wishes = faker.word.noun() + faker.word.adverb() + faker.word.adjective();
+  maxAmount = 50;
+  currency = "Евро";
 
-// When("user logs in", function () {
-// cy.login(users.userAutor.email, users.userAutor.password);
-// });
-
-
-Given("user logs in as {string} and {string}", function (login, password) {
-    cy.login(login, password);
+  cy.contains("Создать коробку").click();
+  cy.get(boxPage.boxNameField).type(newBoxName);
+  cy.get(generalElements.arrowRight).click();
+  cy.get(boxPage.sixthIcon).click();
+  cy.get(generalElements.arrowRight).click();
+  cy.get(boxPage.giftPriceToggle).check({ force: true });
+  cy.get(boxPage.maxAnount).type(maxAmount);
+  cy.get(boxPage.currency).select(currency);
+  cy.get(generalElements.arrowRight).click();
+  cy.get(generalElements.arrowRight).click();
+  cy.get(generalElements.arrowRight).click();
+  cy.get(dashboardPage.createdBoxName).should("have.text", newBoxName);
+  cy.get(".layout-1__header-wrapper-fixed .toggle-menu-item span")
+    .invoke("text")
+    .then((text) => {
+      expect(text).to.include("Участники");
+      expect(text).to.include("Моя карточка");
+      expect(text).to.include("Подопечный");
+    });
 });
 
-Given("user logs in with table", function (dataTable) {
-    cy.log(dataTable.hashes()[0].login, dataTable.hashes()[0].password);
+When("user adds participants", () => {
+  cy.get(generalElements.submitButton).click();
+  cy.get(invitePage.inviteLink)
+    .invoke("text")
+    .then((link) => {
+      inviteLink = link;
+    });
+  cy.clearCookies();
 });
 
-Then("user is on dashboard page", function () {
-    cy.contains("Создать коробку").click();
+Then("each participant confirms participation", (dataTable) => {
+  dataTable.hashes().forEach(({ user, wishes }) => {
+    cy.visit(inviteLink);
+    cy.get(generalElements.submitButton).click();
+    cy.contains("войдите").click();
+    cy.login(user.email, user.password);
+    cy.contains("Создать карточку участника").should("exist");
+    cy.get(generalElements.submitButton).click();
+    cy.get(generalElements.arrowRight).click();
+    cy.get(generalElements.arrowRight).click();
+    cy.get(inviteeBoxPage.wishesInput).type(wishes);
+    cy.get(generalElements.arrowRight).click();
+    cy.get(inviteeDashboardPage.noticeForInvitee)
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.contain("Это — анонимный чат с вашим Тайным Сантой");
+      });
+    cy.clearCookies();
+  });
+});
+
+When("user initiates the draw", () => {
+  cy.visit("/login");
+  cy.login(users.userAutor.email, users.userAutor.password);
+  cy.get(
+    '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
+  ).click();
+  cy.get(':nth-child(4) > a.base--clickable > .user-card').click({ force: true });
+  cy.get('a > .txt-secondary--med').click({ force: true });
+  cy.get('.btn-main').click();
+  cy.get('.santa-modal_content_buttons > .btn-main').click();
+  cy.get('.picture-notice__hint > a > .base--clickable').click();
 });
