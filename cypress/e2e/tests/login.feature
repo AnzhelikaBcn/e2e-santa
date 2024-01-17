3b9dfcc8bb50550a0e569f927a8c91e80a22caa9
Anzhelika <angela.pikulina@mail.com>
2024-01-17 16:19:22 +0100

    HW_7_1_1_End_End

--- cypress/e2e/tests/login.feature
+++ cypress/e2e/tests/login.feature
@@ -1,71 +1,16 @@
-Feature: User can create a box and conduct a draw
+Feature: User login on secret santa website
 
-  Scenario: User creates a box and conducts a draw
-    Given the user navigates to the login page
-    When the user logs in using the credentials of user 1
-    And the user clicks on "Create a box"
-    And the user enters the box name as "newBoxName"
-    And the user selects an icon for the box
-    And the user proceeds to gift settings
-    And the user selects the gift price option
-    And the user enters the maximum number of participants as "50"
-    And the user selects the currency as "Euro"
-    And the user completes the box creation
-    Then the user sees the created box on the dashboard page
-    And the header displays sections "Participants," "My Card," "Ward"
+    Scenario: User logs in sucessfully
+        Given user is on secret santa login page
+        When user logs in as "angela.pikulina+1@gmail.com" and "test1111"
+        #When user logs in with table
+        #| login     | password |
+        #| angela.pikulina+1@gmail.com     | test1111 |
 
-    When the user clicks "Send" to obtain the invitation link
-    And the user clears cookies to simulate a new participant
-
-    When the user follows the invitation link for participant 2
-    And the user clicks "Send"
-    And the user logs in with the credentials of user 2
-    And the user sees the option to "Create a participant card"
-    And the user clicks "Send"
-    And the user proceeds to gift settings
-    And the user enters wishes as "Wishes for user 2"
-    And the user completes the participant card creation
-    Then the user sees the notification "This is an anonymous chat with your Secret Santa"
-    And the user clears cookies
-
-    When the user follows the invitation link for participant 3
-    And the user clicks "Send"
-    And the user logs in with the credentials of user 3
-    And the user sees the option to "Create a participant card"
-    And the user clicks "Send"
-    And the user proceeds to gift settings
-    And the user enters wishes as "Wishes for user 3"
-    And the user completes the participant card creation
-    Then the user sees the notification "This is an anonymous chat with your Secret Santa"
-    And the user clears cookies
-
-    When the user follows the invitation link for participant 4
-    And the user clicks "Send"
-    And the user logs in with the credentials of user 4
-    And the user sees the option to "Create a participant card"
-    And the user clicks "Send"
-    And the user proceeds to gift settings
-    And the user enters wishes as "Wishes for user 4"
-    And the user completes the participant card creation
-    Then the user sees the notification "This is an anonymous chat with your Secret Santa"
-    And the user clears cookies
-
-    When the user logs in with the credentials of user 1
-    And the user clicks "Start the draw"
-    And the user goes to the boxes in the dashboard
-    And the user clicks on the created box
-    And the user clicks on "Start the draw"
-    And the user confirms the draw
-    And the user clicks "Continue"
-    Then the user sees the notification of the draw completion
-    And the user proceeds to the draw summary
-    And the user clicks "Continue"
-    And the user sees the notification "Notification sent to all participants"
-    And the user clicks "Continue"
-    And the user goes to the draw completion
-    And the user clicks on "Return to the boxes"
-    And the user clicks on "Gift"
-    And the user confirms the gift
-    And the user clicks "Continue"
-    Then the user sees the notification "Gift confirmed"
-    And the user completes the draw
+        Then user is on dashboard page
+        Examples:
+            | login                       | password |
+            | angela.pikulina+1@gmail.com | test1111 |
+            | angela.pikulina+2@gmail.com | test1111 |
+            | angela.pikulina+3@gmail.com | test1111 |
+            | angela.pikulina+4@gmail.com | test1111 |
--- cypress/support/step_definitions/login.cy.js
+++ cypress/support/step_definitions/login.cy.js
@@ -2,89 +2,20 @@ import { Given } from "@badeball/cypress-cucumber-preprocessor";
 import { When } from "@badeball/cypress-cucumber-preprocessor";
 import { Then } from "@badeball/cypress-cucumber-preprocessor";
 
-const users = require("../fixtures/users.json");
-const boxPage = require("../fixtures/pages/boxPage.json");
-const generalElements = require("../fixtures/pages/general.json");
-const dashboardPage = require("../fixtures/pages/dashboardPage.json");
-const invitePage = require("../fixtures/pages/invitePage.json");
-const inviteeBoxPage = require("../fixtures/pages/inviteeBoxPage.json");
-const inviteeDashboardPage = require("../fixtures/pages/inviteeDashboardPage.json");
-import { faker } from "@faker-js/faker";
+const users = require("../../fixtures/users.json");
 
-let newBoxName, wishes, maxAmount, currency, inviteLink;
-
-Given("user is logged in", () => {
-  cy.visit("/login");
-  cy.login(users.userAutor.email, users.userAutor.password);
+Given("user is on secret santa login page", function () {
+    cy.visit("/login");
 });
 
-When("user creates a box", () => {
-  newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
-  wishes = faker.word.noun() + faker.word.adverb() + faker.word.adjective();
-  maxAmount = 50;
-  currency = "Евро";
-
-  cy.contains("Создать коробку").click();
-  cy.get(boxPage.boxNameField).type(newBoxName);
-  cy.get(generalElements.arrowRight).click();
-  cy.get(boxPage.sixthIcon).click();
-  cy.get(generalElements.arrowRight).click();
-  cy.get(boxPage.giftPriceToggle).check({ force: true });
-  cy.get(boxPage.maxAnount).type(maxAmount);
-  cy.get(boxPage.currency).select(currency);
-  cy.get(generalElements.arrowRight).click();
-  cy.get(generalElements.arrowRight).click();
-  cy.get(generalElements.arrowRight).click();
-  cy.get(dashboardPage.createdBoxName).should("have.text", newBoxName);
-  cy.get(".layout-1__header-wrapper-fixed .toggle-menu-item span")
-    .invoke("text")
-    .then((text) => {
-      expect(text).to.include("Участники");
-      expect(text).to.include("Моя карточка");
-      expect(text).to.include("Подопечный");
-    });
+Given("user logs in as {string} and {string}", function (login, password) {
+    cy.login(login, password);
 });
 
-When("user adds participants", () => {
-  cy.get(generalElements.submitButton).click();
-  cy.get(invitePage.inviteLink)
-    .invoke("text")
-    .then((link) => {
-      inviteLink = link;
-    });
-  cy.clearCookies();
+When("user logs in with table", function (dataTable) {
+    cy.login(dataTable.hashes()[0].login, dataTable.hashes()[0].password);
 });
 
-Then("each participant confirms participation", (dataTable) => {
-  dataTable.hashes().forEach(({ user, wishes }) => {
-    cy.visit(inviteLink);
-    cy.get(generalElements.submitButton).click();
-    cy.contains("войдите").click();
-    cy.login(user.email, user.password);
-    cy.contains("Создать карточку участника").should("exist");
-    cy.get(generalElements.submitButton).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(inviteeBoxPage.wishesInput).type(wishes);
-    cy.get(generalElements.arrowRight).click();
-    cy.get(inviteeDashboardPage.noticeForInvitee)
-      .invoke("text")
-      .then((text) => {
-        expect(text).to.contain("Это — анонимный чат с вашим Тайным Сантой");
-      });
-    cy.clearCookies();
-  });
-});
-
-When("user initiates the draw", () => {
-  cy.visit("/login");
-  cy.login(users.userAutor.email, users.userAutor.password);
-  cy.get(
-    '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
-  ).click();
-  cy.get(':nth-child(4) > a.base--clickable > .user-card').click({ force: true });
-  cy.get('a > .txt-secondary--med').click({ force: true });
-  cy.get('.btn-main').click();
-  cy.get('.santa-modal_content_buttons > .btn-main').click();
-  cy.get('.picture-notice__hint > a > .base--clickable').click();
+Then("user is on dashboard page", function () {
+    cy.contains("Создать коробку").click();
 });
